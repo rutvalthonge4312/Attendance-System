1107769d6147257ecbf0516bd6c80493c5f01e19
rutvalthonge4312 <127858102+rutvalthonge4312@users.noreply.github.com>
2023-06-16 01:55:41 +0530

    created the show student in admin page

---
+++ showStudentScript.js
@@ -0,0 +1,36 @@
+var apiUrl = "http://localhost/Attendance_API/index.php/user/list?limit=50";
+const request = new Request(apiUrl, {
+  method: "GET",
+});
+fetch(request)
+  .then((response) => response.json())
+  .then((responseJson) => {
+    console.log(JSON.stringify(responseJson));
+    if (responseJson.status == 200) {
+      console.log("hi Data Found!");
+      let tableInShow = document.getElementById("tableInShow");
+      var html;
+      var i = 0;
+
+      html =
+        "<tr><th>Sr.No</th><th>Name</th><th>Roll Number</th><th>Total Attendance</th><th>Division</th>";
+      responseJson.data.forEach((element) => {
+        let trHtml = "<tr>";
+        trHtml += "<td>" + (i + 1) + "</td>";
+
+        trHtml += "<td>" + element.Name + "</td>";
+        trHtml += "<td>" + element.roll + "</td>";
+        trHtml += "<td>" + element.totalAttendance + "</td>";
+        trHtml += "<td>" + element.division + "</td>";
+        trHtml += "</tr>";
+        html += trHtml;
+        i++;
+      });
+      tableInShow.innerHTML = html;
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//
