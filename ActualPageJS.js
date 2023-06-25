e75a629e23ccf4c04f71075b60924820bd16f023
rutvalthonge4312 <127858102+rutvalthonge4312@users.noreply.github.com>
2023-06-25 15:48:48 +0530

    some Changes

--- ActualPageJS.js
+++ ActualPageJS.js
@@ -4,12 +4,19 @@ var allContaint = document.getElementsByClassName(".allContaint");
 var body = document.body;
 var card = document.getElementsByClassName("card");
 
+/*function changeTheBackroundColor(card) {
+  for (var i = 0; i < card.length; i++) {
+    card[i].style.backgroundColor = "red";
+  }
+}*/
+
 darkButton.addEventListener("click", function () {
   body.style.backgroundColor = "black";
   body.style.color = "white";
   for (var i = 0; i < card.length; i++) {
     card[i].style.backgroundColor = "grey";
     card[i].style.color = "white";
+    card[i].style.boxShadow = "2px 2px 4px rgba(0, 0, 0, 0.2)";
   }
 });
 lightButton.addEventListener("click", function () {
@@ -18,5 +25,15 @@ lightButton.addEventListener("click", function () {
   for (var i = 0; i < card.length; i++) {
     card[i].style.backgroundColor = "white";
     card[i].style.color = "black";
+    card[i].style.boxShadow = " 11px 8px 17px 0px rgba(226, 229, 233)";
   }
+});
+Array.from(card).forEach(function (element) {
+  element.addEventListener("mouseenter", function () {
+    element.classList.add("myDivInside");
+  });
+
+  element.addEventListener("mouseleave", function () {
+    element.classList.remove("myDivInside");
+  });
 });
